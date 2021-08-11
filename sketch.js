2439bfc2117af6edd03cd86bcf902dadaf421ff4
PranavB002 <chess.play002@gmail.com>
2021-08-11 11:05:07 -0600

    hello

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 var bird, slingShot;
+var angrybird1, angrybird2
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
